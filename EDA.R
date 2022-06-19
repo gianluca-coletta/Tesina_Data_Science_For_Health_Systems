811b931fc8e9347f5bc9c6fe2ae9d67b98a362e1
Gianluca <coligian9@gmail.com>
2022-06-19 20:11:31 +0200

    Try to summarize and group by month

--- EDA.R
+++ EDA.R
@@ -1,15 +1,37 @@
-# Import dataset 
+# Libraries
 
-dataset <-  read.csv(file = 'influnce_Italy_and_SouthAfrica.csv')
+library(tidyverse)
+library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(lubridate)
+
+# Import dataset of USA Influence report from 2017 to 2021
+
+dataset <-  read.csv(file = 'FluNetInteractiveReport_USA.csv')
 head(dataset)
 
-dataset <- dataset[, -c(1)]
-head(influence)
+# Remove useless columns
+
+dataset <- dataset[, -c(1,2,3,6,8,10,13,22)]
+head(dataset)
+attach(dataset)
 summary(dataset)
 
+# Rename 
 
+# Summarize and group by month
 
-# Delete row with all NA values
-
-dataset <- subset(dataset, !(is.na(dataset$SPEC_RECEIVED_NB))  & !(is.na(dataset$SPEC_PROCESSED_NB)))
-summary(dataset)
+dataset %>% 
+  group_by(month = lubridate::floor_date(EDATE, 'month')) %>%
+  summarize(SPEC_PROCESSED_NB = sum(SPEC_PROCESSED_NB),
+            AH1N12009 = sum(AH1N12009),
+            AH3 = sum(AH3),
+            ANOTSUBTYPED = sum(ANOTSUBTYPED),
+            INF_A = sum(INF_A),
+            BYAMAGATA = sum(BYAMAGATA),
+            BVICTORIA = sum(BVICTORIA),
+            BNOTDETERMINED = sum(BNOTDETERMINED),
+            INF_B = sum(INF_B),
+            ALL_INF = sum(ALL_INF),
+            ALL_INF2 = sum(ALL_INF2))
--- Merge_Dataset.R
+++ Merge_Dataset.R
@@ -1,23 +1,25 @@
-# Dataset of Italy Influence report from 2009 to 2018
+# # Dataset of Italy Influence report from 2009 to 2018
+# 
+# influenceItaly <- read.csv(file = 'FluNetInteractiveReport_Italy.csv')
+# head(influenceItaly)
+# 
+# # Dataset of South Africa Influence report from 2009 to 2018
+# 
+# influenceSouthAfrica <- read.csv(file = 'FluNetInteractiveReport_SouthAfrica.csv')
+# head(influenceSouthAfrica)
+# 
+# # Merge dataset
+# 
+# influence <- rbind(influenceItaly,influenceSouthAfrica)
+# head(influence)
+# attach(influence)
+# 
+# 
+# 
+# write.csv(influence, "C:\\Develop\\Tesina_Data_Science_For_Health_Systems\\influnce_Italy_and_SouthAfrica.csv", row.names=TRUE)
 
-influenceItaly <- read.csv(file = 'FluNetInteractiveReport_Italy.csv')
-head(influenceItaly)
 
-# Dataset of South Africa Influence report from 2009 to 2018
-
-influenceSouthAfrica <- read.csv(file = 'FluNetInteractiveReport_SouthAfrica.csv')
-head(influenceSouthAfrica)
-
-# Merge dataset
-
-influence <- rbind(influenceItaly,influenceSouthAfrica)
-head(influence)
-attach(influence)
-
-# Remove useless columns
-
-influence <- influence[, -c(2,3,5)]
-head(influence)
-summary(influence)
-
-write.csv(influence, "C:\\Develop\\Tesina_Data_Science_For_Health_Systems\\influnce_Italy_and_SouthAfrica.csv", row.names=TRUE)
+# Delete row with all NA values
+# 
+# dataset <- subset(dataset, !(is.na(dataset$SPEC_RECEIVED_NB))  & !(is.na(dataset$SPEC_PROCESSED_NB)))
+# summary(dataset)
