c3f7ab28549a4337f4a9672d3f2b4114c4e04fec
Gianluca <coligian9@gmail.com>
2022-06-19 21:41:00 +0200

    group data by month

--- EDA.R
+++ EDA.R
@@ -13,25 +13,48 @@ head(dataset)
 
 # Remove useless columns
 
-dataset <- dataset[, -c(1,2,3,6,8,10,13,22)]
+dataset <- dataset[, -c(1,2,3,5,6,8,10,13,22)]
 head(dataset)
-attach(dataset)
 summary(dataset)
 
-# Rename 
+# Rename columns
+
+dataset<- dataset %>%
+  rename_with(tolower)
+
+dataset <- dataset %>%
+  rename(date = edate,
+         processed_samples = spec_processed_nb,
+         total_A = inf_a,
+         total_B = inf_b,
+         totale_positve = all_inf,
+         totale_negative = all_inf2)
+
+# Convert column date from character to date
+
+dataset$date <- ymd(dataset$date)
+
+# dataset$date <- as.Date(dataset$date)
 
 # Summarize and group by month
+# 
+# dataset_new <- dataset
+# dataset_new$month <- strftime(dataset_new, )
 
-dataset %>% 
-  group_by(month = lubridate::floor_date(EDATE, 'month')) %>%
-  summarize(SPEC_PROCESSED_NB = sum(SPEC_PROCESSED_NB),
-            AH1N12009 = sum(AH1N12009),
-            AH3 = sum(AH3),
-            ANOTSUBTYPED = sum(ANOTSUBTYPED),
-            INF_A = sum(INF_A),
-            BYAMAGATA = sum(BYAMAGATA),
-            BVICTORIA = sum(BVICTORIA),
-            BNOTDETERMINED = sum(BNOTDETERMINED),
-            INF_B = sum(INF_B),
-            ALL_INF = sum(ALL_INF),
-            ALL_INF2 = sum(ALL_INF2))
+
+
+dataset_new <- dataset %>%
+  group_by(month = lubridate::floor_date(date, 'month')) %>%
+  summarize(processed_samples = sum(processed_samples),
+            ah1n12009 = sum(ah1n12009),
+            ah3 = sum(ah3),
+            anotsubtyped = sum(anotsubtyped),
+            total_A = sum(total_A),
+            byamagata = sum(byamagata),
+            bvictoria = sum(bvictoria),
+            bnotdetermined = sum(bnotdetermined),
+            total_B = sum(total_B),
+            totale_positve = sum(totale_positve),
+            totale_negative = sum(totale_negative))
+
+summary(dataset_new)
