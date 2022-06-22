8ccc799131e67275b3fc0919eb4c2b3c3fe6689f
Gianluca <coligian9@gmail.com>
2022-06-22 17:18:22 +0200

    More plot

--- EDA.R
+++ EDA.R
@@ -5,6 +5,8 @@ library(ggplot2)
 library(magrittr)
 library(dplyr)
 library(lubridate)
+library(reshape2)
+require(gridExtra)
 
 # Import dataset of USA Influence report from 2017 to 2021
 
@@ -67,18 +69,19 @@ dataset$month <- month.abb[as.numeric(dataset$month)]
 # Histogram of the ratio between number of positives and samples processed 
 
 par(mfrow=c(1,2))
+
 hist(dataset$total_positive/dataset$processed_samples,
-     main = "Histogram", 
-     xlab ="Ratio between number of positive and samples processed",
+     main = "Istogramma", 
+     xlab ="Totale positivi / campioni processati",
      breaks = 20)
 
 
 # Histogram of the ratio between number of negative and samples processed 
 
 hist(dataset$total_negative/dataset$processed_samples,
-          main = "Histogram", 
-          xlab = "Ratio between number of negatives and samples processed",
-          breaks = 20)
+          main = "Istogramma", 
+          xlab = "Totale negativi / campioni processati",
+          breaks = 15)
 
 
 # Summary statistics
@@ -92,14 +95,140 @@ dataset_post <- dataset[dataset$year == "2020" | dataset$year == "2021", ]
 
 # Plot total positive cases pre-covid
 
-total_positive_pre <- ggplot(dataset_pre, aes(total_positive_pre)) 
-total_positive_pre 
+total_positive_pre_year <- ggplot(data = dataset_pre,
+                             mapping= aes(x = month(date),
+                                          y = total_positive/processed_samples*100, colour = month))+
+                          geom_point(size = 3) + 
+                          geom_line(colour = "red")+
+                          facet_wrap(~year(date))+
+                          labs(title = "Andamento percentuale positivi per anno")+
+                          theme_bw()
+total_positive_pre_year 
+
+total_positive_pre_month <- ggplot(data = dataset_pre,
+                             mapping= aes(x = year,
+                                          y = total_positive/processed_samples*100))+
+  geom_point(size = 3, alpha = 0.5) + 
+  facet_wrap(~month(date))+
+  labs(title = "Andamento percentuale positivi per mese")+
+  theme_bw()
+
+total_positive_pre_month
+
+# Box-plot for each type of influence pre-covid
+
+# Total A
+dataset_pre %>%
+  ggplot(aes(year,total_A))+
+  geom_boxplot()+
+  theme_bw()+
+  labs(title = "Concentration of influence type-A pre-covid")
+
+# Total B
+dataset_pre %>%
+  ggplot(aes(year,total_B))+
+  geom_boxplot()+
+  theme_bw()+
+  labs(title = "Concentration of influence type-B pre-covid")
+
+
+# Plot total positive cases post-covid
+
+total_positive_post_year <- ggplot(data = dataset_post,
+                                  mapping= aes(x = month(date),
+                                               y = total_positive/processed_samples*100, colour = month))+
+  geom_point(size = 3) + 
+  geom_line(colour = "red")+
+  facet_wrap(~year(date))+
+  labs(title = "Andamento percentuale positivi per anno")+
+  theme_bw()
+total_positive_post_year 
+
+total_positive_post_month <- ggplot(data = dataset_post,
+                                   mapping= aes(x = year,
+                                                y = total_positive/processed_samples*100))+
+  geom_point(size = 3, alpha = 0.5) + 
+  facet_wrap(~month(date))+
+  labs(title = "Andamento percentuale positivi per mese")+
+  theme_bw()
+
+total_positive_post_month
+
+# Box-plot for each type of influence post-covid
+
+# Total A
+dataset_post %>%
+  ggplot(aes(year,total_A))+
+  geom_boxplot()+
+  theme_bw()+
+  labs(title = "Concentration of influence type-A post-covid")
+
+# Total B
+dataset_post %>%
+  ggplot(aes(year,total_B))+
+  geom_boxplot()+
+  theme_bw()+
+  labs(title = "Concentration of influence type-B post-covid")
+
+# Total B without outlier 
+dataset_post %>%
+  filter(total_B<10000) %>%
+  ggplot(aes(year,total_B))+
+  geom_boxplot()+
+  theme_bw()+
+  labs(title = "Concentration of influence type-B post-covid")
+
+
+# Total_positive_pre_year and total_positive_post_year
+
+grid.arrange(total_positive_pre_year,total_positive_post_year,ncol=2)
+
+# Plot of all know type of influence for year pre-covid and post-covid
+
+dataset_pre_types <- dataset_pre[,-c(2,3,4,7,8,11,12,13,14)]
+
+dataset_pre_types <- melt(dataset_pre_types, id.vars = 'date', variable.name = 'type')
+
+plot_type_pre <- dataset_pre_types%>%
+  ggplot(aes(date, value))+
+  geom_point(size = 2)+
+  geom_line(aes(colour = type))
+
+
+dataset_post_types <- dataset_post[,-c(2,3,4,7,8,11,12,13,14)]
+
+dataset_post_types <- melt(dataset_post_types, id.vars = 'date', variable.name = 'type')
+
+plot_type_post <- dataset_post_types%>%
+  ggplot(aes(date, value))+
+  geom_point(size = 2)+
+  geom_line(aes(colour = type))
+
+grid.arrange(plot_type_pre,plot_type_post,ncol=2)
 
 
 
+# plot dataset_pre_types group by year
+
+dataset_pre_types_year <- aggregate(value~year(date)+type, data=dataset_pre_types, FUN=sum) 
+
+dataset_post_types_year <- aggregate(value~year(date)+type, data=dataset_post_types, FUN=sum)
+
+colnames(dataset_pre_types_year) <- c('year','type','value')
+colnames(dataset_post_types_year) <- c('year','type','value')
+
+plot_type_pre_year <- dataset_pre_types_year%>%
+  ggplot(aes(year, value))+
+  geom_point(size = 2)+
+  geom_line(aes(colour = type))
+
+plot_type_post_year <- dataset_post_types_year%>%
+  ggplot(aes(year, value))+
+  geom_point(size = 2)+
+  geom_line(aes(colour = type))
+
+grid.arrange(plot_type_pre_year,plot_type_post_year,ncol=2)
 
 
-
-
-
+###### TEST ######
 
